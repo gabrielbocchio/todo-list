a941556f367829d788dbbcf86649f771f582c073
Gabriel Bocchio <bocchio.gabriel@gmail.com>
2023-07-09 18:31:03 +0200

    improved style when task completed

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,24 +1,24 @@
 import "./list.css"
 
+
 const List = ({list, toggleCompleted, removeItem}) => {
 
   return (
     <>
       <ul className="list">
-        {/* Iterare sulla lista */}
+        {/* Iterare la lista */}
         {list.map((item) => (
-          <li
-            key={item.id}
-            className="task"
-            style={{ textDecoration: item.completed ? "line-through" : "none" }}
-          >
+            <li
+              key={item.id}
+              className={`task ${item.completed ? "completed" : ""}`}
+            >
             <input
               className="checkbox-style"
               type="checkbox"
               checked={item.completed}
               onChange={() => toggleCompleted(item.id)}
             />
-            {item.text}
+             <span>{item.text}</span>
             <button className="erase" onClick={() => removeItem(item.id)}> <i className="fas fa-trash" ></i></button>
           </li>
         ))}
